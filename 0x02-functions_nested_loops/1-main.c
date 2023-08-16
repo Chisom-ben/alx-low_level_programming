9cfdb9a0e420af13ea83120ed607702452276909
chisom-ben <chibenfab@gmail.com>
2023-08-16 13:13:31 -0700

    jhg

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,6 +1,12 @@
- #ifndef MAIN_H
-   #define MAIN_H
-   
-   int _putchar(char c);
-   
-   #endif /* MAIN_H */
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
