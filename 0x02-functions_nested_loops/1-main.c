81b92bf3289df30c664814749015298d326e115c
chisom-ben <chibenfab@gmail.com>
2023-08-16 08:47:45 -0700

    lkj

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,7 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-void print_alphabet(void);
-int _putchar(char c);
-
-#endif /* MAIN_H */
+ #ifndef MAIN_H
+   #define MAIN_H
+   
+   int _putchar(char c);
+   
+   #endif /* MAIN_H */
